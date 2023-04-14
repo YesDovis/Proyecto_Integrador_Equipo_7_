bcd7f537760582df81f69f1d57f35980ebde6535
CloRevollo <clo.revollo@gmail.com>
2023-04-14 03:38:28 -0300

    Se agrega Section Servicios y Contador JS

---
+++ js/counter.js
@@ -0,0 +1,22 @@
+/* CONTADOR JS*/
+
+let valueDisplays = document.querySelectorAll(".number");
+let interval = 5000;
+
+/*console.log(valueDisplays);*/
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+
+    let endValue = parseInt(valueDisplay.getAttribute("data-value"));
+    /*console.log(endValue);*/
+    let duration = Math.floor(interval / endValue);
+
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
